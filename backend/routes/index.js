d6651f088d9a986443cb66d414c17f93eb0a5625
MuqtadirBillah <abbasimuqtadir@gmail.com>
2022-11-09 15:03:08 +0500

    new structure

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const { Router: expressRouter } = require("express");
-const router = expressRouter();
-
-// file routes
-const fileRouter = require("./fileRoutes");
-router.use("/file", fileRouter);
-
-module.exports = router;
